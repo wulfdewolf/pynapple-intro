f7e51333e6b26fee7f16ea65291d363900e8a5c5
wulfdewolf <wolf2wulf@gmail.com>
2026-06-17 00:30:38 +0100

    projects

--- book/load_dandiset.py
+++ book/load_dandiset.py
@@ -1,31 +1,3 @@
----
-toc: false
----
-```{python}
-#| include: false
-exec(open("../settings.py").read())
-```
-
-# Streaming from DANDI {#sec-streaming}
-
-The [DANDI](https://about.dandiarchive.org/) archive allows you to stream data without downloading all the files. 
-
-First, we have to install the `dandi` package:
-```python
-pip install dandi
-```
-
-To start streaming, find the dandiset you are interested in, including the specific file:
-```{python}
-dandiset_id, filepath = (
-    "000939",
-    "sub-A3702/sub-A3702_ses-191126_behavior+ecephys.nwb",
-)
-```
-
-The following function will then stream the data directly into `pynapple`:
-
-```{python}
 import pynapple as nap
 from pynwb import NWBHDF5IO
 from dandi.dandiapi import DandiAPIClient
@@ -33,30 +5,31 @@ import fsspec
 from fsspec.implementations.cached import CachingFileSystem
 import h5py
 
+dandiset_id, filepath = (
+    "000939",
+    "sub-A3702/sub-A3702_ses-191126_behavior+ecephys.nwb",
+)
+
+
 def stream_dandiset(dandiset_id, filepath):
     with DandiAPIClient() as client:
         asset = client.get_dandiset(dandiset_id, "draft").get_asset_by_path(filepath)
         s3_url = asset.get_content_url(follow_redirects=1, strip_query=True)
-    
+
     # first, create a virtual filesystem based on the http protocol
     fs = fsspec.filesystem("http")
-    
+
     # create a cache to save downloaded data to disk (optional)
     fs = CachingFileSystem(
         fs=fs,
         cache_storage="nwb-cache",  # Local folder for the cache
     )
-    
+
     # next, open the file
     file = h5py.File(fs.open(s3_url, "rb"))
     io = NWBHDF5IO(file=file, load_namespaces=True)
 
     return nap.NWBFile(io.read())
-```
 
-Let's call it:
-```{python}
+
 nwbfile = stream_dandiset(dandiset_id, filepath)
-nwbfile
-```
----
--- book/settings.py
+++ book/settings.py
@@ -1,4 +1,8 @@
 import seaborn as sns
 
-custom_params = {"axes.spines.right": False, "axes.spines.top": False, "legend.frameon":False}
+custom_params = {
+    "axes.spines.right": False,
+    "axes.spines.top": False,
+    "legend.frameon": False,
+}
 sns.set_theme(style="ticks", rc=custom_params)
